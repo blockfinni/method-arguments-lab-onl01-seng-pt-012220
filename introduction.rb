d08d82c69663ed9304d74e45d747e5b047b46b67
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-01-28 20:39:15 +0000

    Automatically backed up by Learn

--- introduction.rb
+++ introduction.rb
@@ -1,3 +1,8 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction(name, language)
+  puts "Hi, my name is #{name}, and I am learning programming in #{language}."
 end
+
+name = "AJ"
+language = "Ruby"
+
+introduction(name, language)
